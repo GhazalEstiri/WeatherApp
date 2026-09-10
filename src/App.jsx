429ce0fca7605ec2662bf129b71bc487ba4c7e51
GhazalEstiri <ghazal85.estiri2006@gmail.com>
2026-09-10 18:00:38 +0330

    create weather app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Home from './Components/Home'
+import WeatherDetails from './Components/WeatherDetail'
+function App() {
+
+  return (
+   <>
+   <Home/>
+   <WeatherDetails/>
+   </>
+    
+  )
+}
+
+export default App
---
+++ src/Components/WeatherDetail.jsx
@@ -0,0 +1,6 @@
+function WeatherDetails() {
+    return(
+        <></>
+    )
+}
+export default WeatherDetails
